4a2b34fc22f7803cc387406a78ead472d37fdac6
AndreyBulgakov <pivolit@gmail.com>
2014-10-18 20:21:43 +0400

    new

---
+++ DragonQ/app/src/main/java/com/dragonq/dragonq/Gettarable.java
@@ -0,0 +1,10 @@
+package com.dragonq.dragonq;
+
+import java.util.List;
+
+/**
+ * Created by Андрей on 18.10.2014.
+ */
+public interface Gettarable {
+    List<Question> getQuestion();
+}
---
+++ DragonQ/app/src/main/java/com/dragonq/dragonq/Package.java
@@ -0,0 +1,18 @@
+package com.dragonq.dragonq;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Андрей on 18.10.2014.
+ */
+public class Package implements Gettarable {
+    @Override
+    public List<Question> getQuestion() {
+        List <Question> list = new ArrayList<Question>();
+
+        String url = "";
+
+        return null;
+    }
+}
