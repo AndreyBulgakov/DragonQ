e7bcd438075329c2372588270eef8af315d7529a
AndreyBulgakov <pivolit@gmail.com>
2014-10-18 23:33:49 +0400

    new

--- DragonQ/app/src/main/java/com/dragonq/dragonq/Package.java
+++ DragonQ/app/src/main/java/com/dragonq/dragonq/Package.java
@@ -1,11 +1,14 @@
 package com.dragonq.dragonq;
 
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Андрей on 18.10.2014.
@@ -15,10 +18,14 @@ public class Package implements Gettarable {
     public List<Question> getQuestion() throws IOException {
         List <Question> list = new ArrayList<Question>();
 
-        String url = "";
-
-        Document document = Jsoup.connect(url).get();
-        String jsonfile = document.toString();
+        String url =  "http://rutracker.org/forum/viewtopic.php";
+        String paramValue = "4052102";
+        String paramName = "t";
+        Connection connection = Jsoup.connect(url);
+        Map<String, String> paramsMap = new HashMap<String,String>();
+        paramsMap.put(paramName, paramValue);
+        Document document = connection.data(paramsMap).get();
+        document.body();
 
 
         return null;
